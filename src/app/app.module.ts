1ae7e801aa823dac3389fe2a91e4962ca228065c
David Wilcox <david@custodyscheduler.com>
2017-09-24 17:56:00 -0600

    add ts-config.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,5 +1,46 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { HttpModule } from '@angular/http';
+
+/*
+MdAutocompleteModule,
+MdButtonModule,
+MdButtonToggleModule,
+MdCardModule,
+MdChipsModule,
+MdCheckboxModule,
+MdDialogModule,
+MdGridListModule,
+MdIconModule,
+MdInputModule,
+MdListModule,
+MdMenuModule,
+MdProgressBarModule,
+MdProgressSpinnerModule,
+MdRadioModule,
+MdRippleModule,
+MdSelectModule,
+MdSidenavModule,
+MdSliderModule,
+MdSlideToggleModule,
+MdSnackBarModule,
+MdTabsModule,
+MdToolbarModule,
+MdTooltipModule,
+OverlayModule,
+PortalModule,
+RtlModule,
+StyleModule,
+A11yModule,
+PlatformModule,
+CompatibilityModule,
+ObserveContentModule
+*/
+
+import {  } from '@angular/material';
+import { NoopAnimationsModule } from '@angular/platform-browser/animations';
+import { FlexLayoutModule } from '@angular/flex-layout';
+
 
 import { AppComponent } from './app.component';
 import { NavigationBarComponent } from './navigation-bar/navigation-bar.component';
@@ -13,8 +54,11 @@ import { routing } from './app.routing';
     SplashPageComponent
   ],
   imports: [
-      BrowserModule,
-      routing
+    BrowserModule,
+    FlexLayoutModule,
+    HttpModule,
+    NoopAnimationsModule,
+    routing
   ],
   providers: [],
   bootstrap: [AppComponent]
